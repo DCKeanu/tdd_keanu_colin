e8e514eae42316c3eb74d2879a62fbcc2792aede
De Coster Keanu <dckeanu@hotmail.com>
2024-01-28 18:32:52 +0100

    Partie 1 : R : Ajout d’un test pour vérifier que la fonction sum retourne 0 pour un tableau vide.

--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,7 @@ const sum = require('./sum');
 test('somme des éléments du tableau [1, 2, 3] doit être 6', () => {
     expect(sum([1, 2, 3])).toBe(6);
 });
+
+test('somme des éléments du tableau vide doit être 0', () => {
+    expect(sum([])).toBe(-1);
+});
