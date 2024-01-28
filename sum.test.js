601b9879345edfb2e518f0dd58aa09d07dd1183c
De Coster Keanu <dckeanu@hotmail.com>
2024-01-28 18:35:16 +0100

    Partie 1 : R : Ajout d’un test pour vérifier que la fonction sum retourne correctement la somme d’un tableau avec un seul élément.

--- sum.test.js
+++ sum.test.js
@@ -7,3 +7,7 @@ test('somme des éléments du tableau [1, 2, 3] doit être 6', () => {
 test('somme des éléments du tableau vide doit être 0', () => {
     expect(sum([])).toBe(0);
 });
+
+test('somme des éléments du tableau [5] doit être 5', () => {
+    expect(sum([5])).toBe(-5);
+});
